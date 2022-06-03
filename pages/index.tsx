b2951bf95c026af4b53a76ffe5a5f0918773bdd1
Hesham Abdelhafez <hesham3la2@gmail.com>
2022-06-03 23:43:17 +0300

    internationalization

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,25 @@
 import type { NextPage } from "next";
+import Link from "next/link";
+import Head from "next/head";
+import useTranslation from "next-translate/useTranslation";
 
 type Props = {};
 const Home: NextPage<Props> = ({}) => {
-  return <div></div>;
+  const { t } = useTranslation("common");
+  return (
+    <div>
+      <Head>
+        <title>Hello</title>
+      </Head>
+      <h1>{t("metaTitle")}</h1>
+      <Link href="/" locale="en">
+        <h2>{t("english")}</h2>
+      </Link>
+      <Link href="/" locale="ar">
+        <h2>{t("arabic")}</h2>
+      </Link>
+    </div>
+  );
 };
 
 export default Home;
